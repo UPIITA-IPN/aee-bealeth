767a61f5b7b625f1ba72d9a7d3e2f8a4daa9202d
Bea <94203492+bealeth@users.noreply.github.com>
2026-03-11 04:21:38 +0000

    fix autograder

--- aee.py
+++ aee.py
@@ -1,3 +1,5 @@
+import sys
+
 def euclides_extendido(a, b):
     x0, x1 = 1, 0
     y0, y1 = 0, 1
@@ -10,6 +12,7 @@ def euclides_extendido(a, b):
 
     return a, x0, y0
 
+
 def inverso_modulo(a, m):
     mcd, x, _ = euclides_extendido(a, m)
     if mcd != 1:
@@ -17,30 +20,14 @@ def inverso_modulo(a, m):
     return x % m
 
 
-print("Algoritmo extendido de Euclides")
+# input
+entrada = sys.stdin.read().strip().split()
+a = int(entrada[0])
+b = int(entrada[1])
 
-try:
-    # Entrada de datos
-    a = int(input("Ingresa el primer número (a): "))
-    b = int(input("Ingresa el segundo número (b): "))
+inv = inverso_modulo(a, b)
 
-    # Algoritmo extendido
-    gcd, x, y = euclides_extendido(a, b)
-
-    print("\nResultados del algoritmo extendido:")
-    print("gcd(a, b):", gcd)
-    print("x:", x)
-    print("y:", y)
-    print("Verificación:", a * x + b * y)
-
-    # Inverso multiplicativo de a mod b
-    inv = inverso_modulo(a, b)
-
-    if inv is None:
-        print(f"\nNo existe inverso multiplicativo de {a} módulo {b}")
-    else:
-        print(f"\nInverso multiplicativo de {a} módulo {b} =", inv)
-        print("Verificación:", (a * inv) % b)
-
-except ValueError:
-    print("\nError: Debes ingresar números enteros válidos.")
+if inv is None:
+    print("No existe inverso")
+else:
+    print(inv)
